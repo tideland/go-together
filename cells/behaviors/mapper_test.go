000ac5d95178b085648142d6aaffbc97d5cf21a4
Frank Mueller <mail@themue.dev>
2020-02-06 21:27:03 +0100

    Improve behavior testing
    Signed-off-by: Frank Mueller <mail@themue.dev>

--- cells/behaviors/mapper_test.go
+++ cells/behaviors/mapper_test.go
@@ -13,7 +13,6 @@ package behaviors_test // import "tideland.dev/go/together/cells/behaviors"
 
 import (
 	"strings"
-	"sync"
 	"testing"
 
 	"tideland.dev/go/audit/asserts"
@@ -29,42 +28,26 @@ import (
 // TestMapperBehavior tests the mapping of events.
 func TestMapperBehavior(t *testing.T) {
 	assert := asserts.NewTesting(t, asserts.FailStop)
-	msh := mesh.New()
-
-	var wg sync.WaitGroup
-
 	mapper := func(evt *event.Event) (*event.Event, error) {
 		text := evt.Payload().At("text").AsString("")
 		return event.New(evt.Topic(), "text", strings.ToUpper(text)), nil
 	}
-	processor := func(emitter mesh.Emitter, evt *event.Event) error {
-		wg.Done()
-		text := evt.Payload().At("text").AsString("")
-		switch evt.Topic() {
-		case "a":
-			assert.Equal(text, "ABC")
-		case "b":
-			assert.Equal(text, "DEF")
-		case "c":
-			assert.Equal(text, "GHI")
-		default:
-			assert.Fail("mapper didn't work: %s = %s", evt.Topic(), text)
-		}
-		return nil
-	}
+	plant := mesh.NewTestPlant(assert, behaviors.NewMapperBehavior("mb", mapper), 1)
+	defer plant.Stop()
 
-	assert.OK(msh.SpawnCells(
-		behaviors.NewMapperBehavior("mapper", mapper),
-		behaviors.NewSimpleProcessorBehavior("processor", processor),
-	))
-	assert.OK(msh.Subscribe("mapper", "processor"))
+	plant.Emit(event.New("a", "text", "abc"))
+	plant.Emit(event.New("b", "text", "def"))
+	plant.Emit(event.New("c", "text", "ghi"))
 
-	wg.Add(3)
-	assert.OK(msh.Emit("mapper", event.New("a", "text", "abc")))
-	assert.OK(msh.Emit("mapper", event.New("b", "text", "def")))
-	assert.OK(msh.Emit("mapper", event.New("c", "text", "ghi")))
-	wg.Wait()
-	assert.OK(msh.Stop())
+	plant.AssertFind(0, func(evt *event.Event) bool {
+		return evt.Topic() == "a" && evt.Payload().At("text").AsString("-") == "ABC"
+	})
+	plant.AssertFind(0, func(evt *event.Event) bool {
+		return evt.Topic() == "b" && evt.Payload().At("text").AsString("-") == "DEF"
+	})
+	plant.AssertFind(0, func(evt *event.Event) bool {
+		return evt.Topic() == "c" && evt.Payload().At("text").AsString("-") == "GHI"
+	})
 }
 
 // EOF
